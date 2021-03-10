1d2b10c391b3ddfc3437aa97108274619b8dcdcb
Evgeniy Dzhumak <elforastero@ya.ru>
2021-03-10 22:35:13 +0400

    feat: easy includes

---
+++ src/easy/includes/index.ts
@@ -0,0 +1,34 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #easy #array
+
+  ### Question
+
+  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments. The output should be a boolean `true` or `false`.
+  For example
+  ```
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+
+  > View on GitHub: https://tsch.js.org/898
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Includes<T extends readonly any[], U> = U extends T[number] ? true : false;
+
+type Test = Includes<[1, 2, 3], 3>;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils';
+
+type cases = [
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>
+  >,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>
+];
