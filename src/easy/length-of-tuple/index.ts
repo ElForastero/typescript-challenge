493100317cedad0ee542fee9653eaca924a74e20
Evgeniy Dzhumak <elforastero@ya.ru>
2021-03-10 22:35:26 +0400

    feat: easy length-of-tuple

---
+++ src/easy/length-of-tuple/index.ts
@@ -0,0 +1,49 @@
+/*
+  18 - Length of Tuple
+  -------
+  by sinoon (@sinoon) #easy #tuple
+
+  ### Question
+
+  For given a tuple, you need create a generic `Length`, pick the length of the tuple
+
+  For example
+
+  ```ts
+  type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+  type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+  type teslaLength = Length<tesla>  // expected 4
+  type spaceXLength = Length<spaceX> // expected 5
+  ```
+
+  > View on GitHub: https://tsch.js.org/18
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// Naive solution
+// type Length<T extends readonly any[]> = T['length'];
+
+// Better solution
+// type Length<T extends any> = T extends ReadonlyArray<any> ? T['length'] : never;
+
+// 🤔
+type Length<T extends any> = T extends { length: infer U } ? U : never;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils';
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const spaceX = [
+  'FALCON 9',
+  'FALCON HEAVY',
+  'DRAGON',
+  'STARSHIP',
+  'HUMAN SPACEFLIGHT',
+] as const;
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>
+];
